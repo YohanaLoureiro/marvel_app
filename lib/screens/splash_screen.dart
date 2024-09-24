17d6a9686282f7af983f7a8cf0f535e9d6c826ce
Yohana Dhara <yohanaloureiro@gmail.com>
2024-09-24 19:18:09 -0300

    feat: adicionar tela SplashScreen
    Adiciona a tela SplashScreen ao aplicativo, que exibe o logotipo da Marvel por 3 segundos antes de navegar para a HomeScreen.

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  void _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 3));
+
+    // Verifica se o widget ainda está montado antes de navegar
+    if (mounted) {
+      Navigator.pushReplacementNamed(context, '/home');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0x00202020),
+      body: Center(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            double logoSize = constraints.maxWidth * 0.5;
+            return Image.asset(
+              'assets/images/Marvel_Logo.png',
+              width: logoSize,
+              height: logoSize,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
